da58cd7abf783628010aa160aed1fe772fff2309
atulrajdhar <atulrajdhar@users.noreply.github.com>
2022-11-20 13:29:24 +0530

    Added ArticlePage and ArticleViewModel

--- NewsApp/Views/ArticlePage.xaml.cs
+++ NewsApp/Views/ArticlePage.xaml.cs
@@ -1,9 +1,23 @@
+using NewsApp.Models;
+using NewsApp.Services;
+using NewsApp.ViewModels;
+
 namespace NewsApp.Views;
 
-public partial class ArticlePage : ContentPage
+public partial class ArticlePage : ContentPage, IQueryAttributable
 {
-	public ArticlePage()
+	private INewsService _news;
+	public ArticlePage(INewsService news)
 	{
+		_news = news;
 		InitializeComponent();
+	}
+
+	public void ApplyQueryAttributes(IDictionary<string, object> query)
+	{
+		if (query.FirstOrDefault(a => a.Key.Equals("article")).Value is Article a)
+		{
+			this.BindingContext = new ArticleViewModel(_news, a);
+		}
 	}
 }
